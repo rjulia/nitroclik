29e0bd00e50f93acc3d0170949534cf7ed77dee3
Ramon Julia <reimon1972@gmail.com>
2019-02-20 12:20:50 +0800

    using nwes hooks

--- src/components/image/ResponsiveImage.js
+++ src/components/image/ResponsiveImage.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 
-class ResponsiveImage extends React.Component {
-  
-  render () {
-    const { small, medium, large, xlarge } = this.props.images; 
-    let sizeImge;
-    const size = document.body.clientWidth;
-    switch (true) {
-      case (size > 1280):
-        sizeImge = xlarge
-        break;
-      case (size > 768):
-        sizeImge = large
-        break;
-      case (size > 376):
-        sizeImge = medium
-        break;
-      default:
-        sizeImge = small
-        break;
+export default  function ResponsiveImage(props) {
+  const [width, setWidth] = useState(0);
+  const { small, medium, large, xlarge } = props.images; 
+  let sizeImge;
+  useEffect(() => {
+    // creamos una función para actualizar el estado con el clientWidth
+    const updateWidth = () => {
+      const width = document.body.clientWidth
+      setWidth(width)
     }
-    return (
-        <img {...this.props} src={sizeImge} alt="nothing"/>
-    );
+    // actualizaremos el width al montar el componente
+    updateWidth()
+    // nos suscribimos al evento resize de window
+    window.addEventListener("resize", updateWidth)
+  })
+  switch (true) {
+    case (width > 1280):
+      sizeImge = xlarge
+      break;
+    case (width > 768):
+      sizeImge = large
+      break;
+    case (width > 376):
+      sizeImge = medium
+      break;
+    default:
+      sizeImge = small
+      break;
   }
+
+  return (
+      <img {...props} src={sizeImge} alt="nothing"/>
+  );
+  
 }
 
-export default ResponsiveImage;
